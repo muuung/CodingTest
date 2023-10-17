8b978417279a71e84ecc631463fc0e1dd86fc647
muuung <caninespaw@gmail.com>
2023-10-17 23:01:34 +0900

    롤케이크 자르기 (통과)

--- programmers/src/step2/P132265.java
+++ programmers/src/step2/P132265.java
@@ -1,21 +1,19 @@
 package step2;
 import java.util.*;
 
-/* 롤케이크 자르기 (풀이중) */
+/* 롤케이크 자르기 (통과) */
 public class P132265 {
     public int solution(int[] topping) {
     	int answer = 0;
-    	HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         HashSet<Integer> set = new HashSet<>();
         
-        for(int i = 0; i < topping.length; i++) {
-        	map.put(topping[i], map.getOrDefault(topping[i], 0));
-        }
+        for(int i : topping)
+        	map.put(i, map.getOrDefault(i, 0)+1);
         
-        for(int i = 0; i < topping.length-1; i++) {
-        	map.put(topping[i], map.get(topping[i])-1);
-        	set.add(topping[i]);
-        	if(map.get(topping[i]) == 0) map.remove(topping[i]);
+        for(int i : topping) {
+        	if(map.replace(i, map.get(i)-1) == 1) map.remove(i);
+        	set.add(i);
         	if(map.size() == set.size()) answer++;
         }
         
