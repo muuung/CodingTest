d6203feeaba7e49a2d4aef8a1b5140abf870ee62
muuung <caninespaw@gmail.com>
2023-10-30 22:37:25 +0900

    추억 점수 (통과)

---
+++ programmers/src/step1/P176963.java
@@ -0,0 +1,22 @@
+package step1;
+import java.util.*;
+
+/* 추억 점수 */
+public class P176963 {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        
+        for(int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
+        for(int i = 0; i < photo.length; i++) {
+            for(int j = 0; j < photo[i].length; j++) {
+                answer[i] += map.getOrDefault(photo[i][j], 0);
+            }
+        }
+        
+        return answer;
+    }
+}
--- programmers/src/step1/run.java
+++ programmers/src/step1/run.java
@@ -1,12 +1,18 @@
 package step1;
+import java.util.*;
 
 public class run {
 	public static void main(String[] args) {
-		P12928 p = new P12928();
+//		P12928 p = new P12928();
 //		P12944 p = new P12944();
+		P176963 p = new P176963();
 		
 		try {
-			System.out.println(p.solution(8));
+			String[] arr1 = {"may", "kein", "kain", "radi"};
+			int[] arr2 = {5, 10, 1, 3};
+			String[][] arr3 = {{"may"},{"kein", "deny", "may"}, {"kon", "coni"}};			
+			int[] answer = p.solution(arr1, arr2, arr3);
+			System.out.print(Arrays.toString(answer));
 		} catch(Exception e) {
 			System.out.println("ERROR : " + e.getMessage());
 		}
