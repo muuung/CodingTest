929f3883833c9a3299efb2c5702e4a658f69289a
muuung <caninespaw@gmail.com>
2023-11-29 23:08:22 +0900

    제일 작은 수 제거하기 (통과)

--- programmers/src/step1/P12935.java
+++ programmers/src/step1/P12935.java
@@ -1,14 +1,25 @@
 package step1;
 import java.util.Arrays;
 
-/* 제일 작은 수 제거하기 */
+/* 제일 작은 수 제거하기 (통과) */
 public class P12935 {
     public int[] solution(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-        int min = 0;
+        int empty[] = {-1};
+        if(arr.length == 1) return empty;
         
-        if(arr.length == 1) {
-            return 
+        int[] answer = new int[arr.length-1];
+        int aIdx = 0;
+        int idx = 0;
+        int min = Arrays.stream(arr).min().getAsInt();
+        
+        while(idx < arr.length) {
+            if(arr[idx] == min) {
+                idx++;
+                continue;
+            }
+            answer[aIdx] = arr[idx];
+            aIdx++;
+            idx++;
         }
         
         return answer;
