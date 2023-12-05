3c3c65dac680f2fbfc95312952af8c41a10f0b54
muuung <caninespaw@gmail.com>
2023-12-05 23:46:06 +0900

    가운데 글자 가져오기 (통과)

--- programmers/src/step1/P12903.java
+++ programmers/src/step1/P12903.java
@@ -1,13 +1,11 @@
 package step1;
 
+/* 가운데 글자 가져오기 (통과) */
 public class P12903 {
     public String solution(String s) {
-        String answer = s.charAt(s.length()/2);
-        
-        if(s.length()%2 == 0) {
-            answer = s.substring(s.length()/2-1, s.length()/2+1);
-        }
-        
+        String answer = String.valueOf(s.charAt((s.length()-1)/2));
+        if(s.length()%2 == 0)
+            answer += String.valueOf(s.charAt(s.length()/2));
         return answer;
     }
 }
