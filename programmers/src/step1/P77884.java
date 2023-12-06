aba8077d5e8084162866a03c2f125ca18d7cff76
muuung <caninespaw@gmail.com>
2023-12-06 20:06:03 +0900

    약수의 개수와 덧셈 (통과)

--- programmers/src/step1/P77884.java
+++ programmers/src/step1/P77884.java
@@ -1,22 +1,27 @@
 package step1;
 import java.util.*;
 
-/* 약수의 개수와 덧셈 (풀이중) */
+/* 약수의 개수와 덧셈 (통과) */
 public class P77884 {
     public int solution(int left, int right) {
         int answer = 0;
         Set<Integer> set = new HashSet<>();
         
-        // 약수의 개수
         for(; left <= right; left++) {
-            for(int i = 1; i <= left; i++) {
-                if(set.contains(i)) {
-                    break;
-                } else if(left%i == 0) {
+            for(int i = 1; i <= Math.sqrt(left); i++) {
+                if(left%i == 0) {
                     set.add(i);
-                    set.add(left/i);
+                    set.add(left/i);   
                 }
             }
+            
+            if(set.size()%2 == 0) {
+                answer += left;
+            } else {
+                answer -= left;
+            }
+            
+            set.clear();
         }
         
         return answer;
