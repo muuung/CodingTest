30c8cd2eb0a2be825f949e5ee7ee712bfa80d256
muuung <caninespaw@gmail.com>
2024-02-02 23:07:15 +0900

    2024-02-02
    내적 (통과)

---
+++ programmers/src/step1/P70128.java
@@ -0,0 +1,13 @@
+package step1;
+
+/* 내적 (통과) */
+public class P70128 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        
+        for(int i = 0; i < a.length; i++)
+            answer += a[i]*b[i];
+            
+        return answer;
+    }
+}
