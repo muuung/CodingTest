eb21efa7cb047ecfb5b6c55fae853ce0f4903eea
muuung <caninespaw@gmail.com>
2024-02-04 23:56:02 +0900

    2024-02-04
    문자열 내 p와 y의 개수 (풀이중)

---
+++ programmers/src/step1/P12916.java
@@ -0,0 +1,16 @@
+package step1;
+
+/* 문자열 내 p와 y의 개수 (풀이중) */
+public class P12916 {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        s.replace("P", "");
+        
+        int len1 = s.replace("p", "").length();
+        
+        
+        
+        return answer;
+    }
+}
