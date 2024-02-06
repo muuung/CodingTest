99e2ec4bff7dfb2ca1742a802b79ba43ae6f3375
muuung <caninespaw@gmail.com>
2024-02-06 22:37:51 +0900

    2024-02-06
    문자열 내 p와 y의 개수 (통과)

--- programmers/src/step1/P12916.java
+++ programmers/src/step1/P12916.java
@@ -1,14 +1,16 @@
 package step1;
 
-/* 문자열 내 p와 y의 개수 (풀이중) */
+/* 문자열 내 p와 y의 개수 (통과) */
 public class P12916 {
     boolean solution(String s) {
     	int p = 0;
     	int y = 0;
 
         for(int i = 0; i < s.length(); i++) {
-        	if(s.charAt(i).equals("p")) {
-        		
+        	if(s.charAt(i) == 'p' || s.charAt(i) == 'P') {
+        		p++;
+        	} else if(s.charAt(i) == 'y' || s.charAt(i) == 'Y') {
+        		y++;
         	}
         }
         
