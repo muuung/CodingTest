a09e5346259ec637d9a4e599ebdd815a913d1098
muuung <caninespaw@gmail.com>
2024-02-15 20:54:39 +0900

    2024-02-15
    콜라츠 추측 (통과)

--- programmers/src/step1/P12943.java
+++ programmers/src/step1/P12943.java
@@ -1,22 +1,19 @@
 package step1;
 
-/* 콜라츠 추측 (풀이중) */
+/* 콜라츠 추측 (통과) */
 public class P12943 {
     public int solution(int num) {
-        int answer = 0;
+        long cal = num;
+        int cnt = 0;
         
-        while(num != 1) {
-            if(answer == 500) {
-                answer = -1;
-                break;
-            } else if(num%2 == 0) {
-                num /= 2;
-            } else {
-                num = num*3 + 1; // 여기서 int형의 범위를 벗어날 수 있으므로 long형 변수를 사용해야한다..?
-            }
-            answer++;
+        while(cal != 1 && cnt != 500) {
+            if(cal%2 == 0)
+                cal /= 2;
+            else
+                cal = cal*3 + 1;
+            cnt++;
         }
         
-        return answer;
+        return cnt < 500? cnt : -1;
     }
 }
