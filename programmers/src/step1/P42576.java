91c2ed33a671c65b5be7da51c23cfb7fb3f8fbca
muuung <caninespaw@gmail.com>
2024-02-27 17:53:46 +0900

    2024-02-27
    완주하지 못한 선수 (통과)

---
+++ programmers/src/step1/P42576.java
@@ -0,0 +1,35 @@
+package step1;
+import java.util.*;
+
+/* 완주하지 못한 선수 (통과) */
+public class P42576 {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+
+        for(i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i]))
+                return participant[i];
+        }
+
+        return participant[i];
+
+//        다른 풀이 ↓↓
+//
+//        String answer = "";
+//        HashMap<String, Integer> map = new HashMap<>();
+//
+//        for(String key : participant) map.put(key, map.getOrDefault(key, 0) + 1);
+//        for(String key : completion)  map.put(key, map.get(key) - 1);
+//
+//        for(String key : map.keySet()) {
+//            if(map.get(key) != 0) {
+//                answer = key;
+//                break;
+//            }
+//        }
+//
+//        return answer;
+    }
+}
