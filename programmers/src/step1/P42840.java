5c60bd5cc6a99622801e272c015febd3f41ee779
muuung <caninespaw@gmail.com>
2024-03-02 01:57:10 +0900

    2024-03-02
    모의고사 (풀이중)

--- programmers/src/step1/P42840.java
+++ programmers/src/step1/P42840.java
@@ -1,23 +1,49 @@
 package step1;
-import java.util.Arrays;
+import java.util.*;
+import java.util.Map.*;
 
 /* 모의고사 (풀이중) */
 public class P42840 {
     public int[] solution(int[] answers) {
-        int idx1 = 0;
-        int idx2 = 0;
-        int idx3 = 0;
-
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int cnt3 = 0;
-
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        for(int i = 0; i < answers.length; i++) {
-            if(arr1[idx1] == answers[i]) cnt++;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, count(arr1, answers));
+        map.put(2, count(arr2, answers));
+        map.put(3, count(arr3, answers));
+
+        List<Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Entry.comparingByValue());
+
+        // 여기서 오류..ㅜㅜ
+        for(int i = 1; i < 3; i++) {
+            if(!list.get(0).getValue().equals(list.get(i).getValue())) {
+                list.remove(i);
+                i--;
+            }
         }
+
+        int[] answer = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i).getKey();
+        }
+
+        return answer;
+    }
+
+    int count(int[] arr, int[] answers) {
+        int idx = 0;
+        int cnt = 0;
+
+        for(int i = 0; i < answers.length; i++) {
+            if(arr.length == idx) idx = 0;
+            if(arr[idx] == answers[i]) cnt++;
+            idx++;
+        }
+
+        return cnt;
     }
 }
