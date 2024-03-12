9ff69f9c7e2435cbe0305ea8a5a98fa7db406fcd
muuung <caninespaw@gmail.com>
2024-03-12 10:23:32 +0900

    2024-03-12
    크기가 작은 부분 문자열 (통과)

--- programmers/src/step1/P147355.java
+++ programmers/src/step1/P147355.java
@@ -1,18 +1,17 @@
 package step1;
 
-/* 크기가 작은 부분 문자열 (풀이중) */
+/* 크기가 작은 부분 문자열 (통과) */
 public class P147355 {
     public int solution(String t, String p) {
-        double t2 = 0;
-        double p2 = Integer.parseInt(p);
-        int len = p.length()-1;
-        int answer = 0;
-        
+        int cnt = 0;
+        int len = p.length();
+        long p2 = Long.parseLong(p);
+
         for(int i = 0; i <= t.length()-len; i++) {
-            t2 = Integer.parseInt(t.substring(i, i+len));
-            if(t2 <= p2) answer++;
+            if(Long.parseLong(t.substring(i, i+len)) <= p2)
+                cnt++;
         }
-        
-        return answer;
+
+        return cnt;
     }
 }	
