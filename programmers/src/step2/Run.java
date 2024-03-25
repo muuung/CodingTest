4a8cb26b96fba4f7d5b63342108ecbdd4c117a20
muuung <caninespaw@gmail.com>
2024-03-25 11:51:51 +0900

    2024-03-25

--- programmers/src/step2/Run.java
+++ programmers/src/step2/Run.java
@@ -7,10 +7,13 @@ public class Run {
 //		P49993 p = new P49993();
 //		P42885 p = new P42885();
 //		P132265 p = new P132265();
-		P12924 p = new P12924();
-		
+//		P12924 p = new P12924();
+		P42577 p = new P42577();
+
+		String[] arr = {"123","456","789"};
+
 		try {
-			System.out.println(p.solution(15));
+			System.out.println(p.solution(arr));
 		} catch(Exception e) {
 			System.out.println("ERROR : " + e.getMessage());
 		}		
