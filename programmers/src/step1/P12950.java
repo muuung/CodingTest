d57b3d18ed5e628b465173f39ac043dfa191c14e
muuung <caninespaw@gmail.com>
2024-03-28 21:52:30 +0900

    2024-03-28
    행렬의 덧셈 (통과)

---
+++ programmers/src/step1/P12950.java
@@ -0,0 +1,12 @@
+package step1;
+
+/* 행렬의 덧셈 (통과) */
+public class P12950 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        for(int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr1[i].length; j++)
+                arr1[i][j] += arr2[i][j];
+        }
+        return arr1;
+    }
+}
