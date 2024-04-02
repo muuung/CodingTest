2835963750b02bfb794639072c784e6dba89b45b
muuung <caninespaw@gmail.com>
2024-04-02 21:38:28 +0900

    2024-04-02
    두 원 사이의 정수 쌍 (통과)

--- programmers/src/step2/P181187.java
+++ programmers/src/step2/P181187.java
@@ -1,8 +1,9 @@
 package step2;
 
+/* 두 원 사이의 정수 쌍 (통과) */
 public class P181187 {
     public long solution(int r1, int r2) {
-        double r12 = Math.pow(r1, 2);
+        double r11 = Math.pow(r1, 2);
         double r22 = Math.pow(r2, 2);
         double d = 0, y = 0;
         long answer = 0;
@@ -12,12 +13,24 @@ public class P181187 {
             y = Math.sqrt(r22 - d);
 
             if(x >= r1) {
-                answer += (int)y;
+                answer += (long)y;
             } else {
-                // 코드 추가 예정
+                answer += Math.floor(y) - Math.ceil(Math.sqrt(r11 - d)) + 1;
             }
         }
 
         return answer * 4;
+
+//        다른 풀이 ↓↓
+//
+//        long answer = 0, y1 = 0, y2 = 0;
+//
+//        for(int x = 1; x <= r2; x++) {
+//            y1 = (long)Math.ceil(Math.sqrt((long)r1 * r1 - (long)x * x));
+//            y2 = (long)Math.floor(Math.sqrt((long)r2 * r2 - (long)x * x));
+//            answer += y2 - y1 + 1;
+//        }
+//
+//        return answer * 4;
     }
 }
