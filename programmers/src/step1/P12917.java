e4bfccb7ae469f1b04db361da48cf6e250114a90
muuung <caninespaw@gmail.com>
2024-04-14 00:11:22 +0900

    2024-04-14
    문자열 내림차순으로 배치하기 (통과)

--- programmers/src/step1/P12917.java
+++ programmers/src/step1/P12917.java
@@ -1,5 +1,16 @@
 package step1;
+import java.util.Arrays;
 
-/* 문자열 내림차순으로 배치하기 (풀이중) */
+/* 문자열 내림차순으로 배치하기 (통과) */
 public class P12917 {
+    public String solution(String s) {
+        String[] arr = s.split("");
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = arr.length-1; i >= 0; i--)
+            sb.append(arr[i]);
+
+        return sb.toString();
+    }
 }
