a471585b3f7fa11f442136aa3d17f3bf42674d78
muuung <caninespaw@gmail.com>
2024-04-17 16:37:46 +0900

    2024-04-17
    의상 (통과)

---
+++ programmers/src/step2/P42578.java
@@ -0,0 +1,18 @@
+package step2;
+import java.util.*;
+
+/* 의상 (통과) */
+public class P42578 {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String[] cloth : clothes)
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+
+        for(Integer value : map.values())
+            answer *= value + 1;
+
+        return answer - 1;
+    }
+}
