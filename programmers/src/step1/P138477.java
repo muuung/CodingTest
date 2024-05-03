ef4b6e025356e585664ead4f36db9e9301b2ab10
muuung <caninespaw@gmail.com>
2024-05-03 23:57:21 +0900

    2024-05-03
    명예의 전당 (풀이중)

--- programmers/src/step1/P138477.java
+++ programmers/src/step1/P138477.java
@@ -1,23 +1,24 @@
 package step1;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+/* 명예의 전당 (풀이중) */
 public class P138477 {
     public int[] solution(int k, int[] score) {
         int len = score.length;
         int[] answer = new int[len];
 
         List<Integer> list = new ArrayList<>();
+        list.add(score[0]);
 
         for(int i = 0; i < len; i++) {
-            if(list.size() == k) {
-                if(list.get(0) < score[i]) {
-                    list.remove(0);
-                }
-            }
+            answer[i] = list.get(0);
 
+            if(list.size() == k) {
+                if(list.get(0) < score[i])
+                    list.remove(0);
+                else
+                    continue;
+            }
 
             list.add(score[i]);
             Collections.sort(list);
