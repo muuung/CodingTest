0a22c54daafb53a10a9c42f66e680ca18af00e3c
muuung <caninespaw@gmail.com>
2024-05-04 00:52:12 +0900

    2024-05-04
    명예의 전당 (통과)

--- programmers/src/step1/P138477.java
+++ programmers/src/step1/P138477.java
@@ -1,27 +1,27 @@
 package step1;
 import java.util.*;
 
-/* 명예의 전당 (풀이중) */
+/* 명예의 전당 (통과) */
 public class P138477 {
     public int[] solution(int k, int[] score) {
         int len = score.length;
         int[] answer = new int[len];
-
         List<Integer> list = new ArrayList<>();
-        list.add(score[0]);
 
         for(int i = 0; i < len; i++) {
-            answer[i] = list.get(0);
-
             if(list.size() == k) {
-                if(list.get(0) < score[i])
+                if(list.get(0) < score[i]) {
                     list.remove(0);
-                else
+                }
+                else {
+                    answer[i] = list.get(0);
                     continue;
+                }
             }
 
             list.add(score[i]);
             Collections.sort(list);
+            answer[i] = list.get(0);
         }
 
         return answer;
