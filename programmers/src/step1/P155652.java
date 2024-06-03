ba5b3c0d18a1f90a40f7c5c352dd93b91b42a22f
muuung <caninespaw@gmail.com>
2024-06-03 23:53:54 +0900

    2024-06-03

---
+++ programmers/src/step1/P155652.java
@@ -0,0 +1,12 @@
+package step1;
+
+/* 둘만의 암호 (풀이중) */
+public class P155652 {
+    public String solution(String s, String skip, int index) {
+        char a = 'a';
+        a += 1;
+        System.out.println(a);
+        String answer = "";
+        return answer;
+    }
+}
