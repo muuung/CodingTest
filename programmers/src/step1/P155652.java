96be629b6980ab0615932f675d26d75cd6267b4d
muuung <caninespaw@gmail.com>
2024-06-04 23:47:05 +0900

    2024-06-04

--- programmers/src/step1/P155652.java
+++ programmers/src/step1/P155652.java
@@ -5,6 +5,9 @@ public class P155652 {
     public String solution(String s, String skip, int index) {
         char a = 'a';
         a += 1;
+
+        for(int i = 0; i < index; i++) {
+        }
         System.out.println(a);
         String answer = "";
         return answer;
