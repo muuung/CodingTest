0edc8e215b662d64abaaf561525597b9e494e3d6
muuung <caninespaw@gmail.com>
2024-06-13 21:49:21 +0900

    2024-06-13
    둘만의 암호 (통과)

--- programmers/src/step1/P155652.java
+++ programmers/src/step1/P155652.java
@@ -1,15 +1,22 @@
 package step1;
 
-/* 둘만의 암호 (풀이중) */
+/* 둘만의 암호 (통과) */
 public class P155652 {
     public String solution(String s, String skip, int index) {
-        char a = 'a';
-        a += 1;
+        char c;
+        StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < index; i++) {
+        for(int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            for(int j = 0; j < index; j++) {
+                if(c++ == 'z')
+                    c = 'a';
+                if(skip.indexOf(c) != -1)
+                    j--;
+            }
+            sb.append(c);
         }
-        System.out.println(a);
-        String answer = "";
-        return answer;
+
+        return sb.toString();
     }
 }
